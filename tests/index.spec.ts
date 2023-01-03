2bc36a24c347dc7e8835bc4de397eb03e757c3a2
Faris CHTATOU <faris.manage@gmail.com>
2023-01-03 11:18:50 +0100

    add utils and tests

---
+++ tests/index.spec.ts
@@ -0,0 +1,78 @@
+import { test, expect, type Page } from "@playwright/test";
+
+import { createNewTeam, createNewUser } from "../utils";
+
+const NEW_TEAM_NAME = "Test";
+const EXISTING_TEAM_NAME = NEW_TEAM_NAME;
+
+const SQL_INJECTION = `'; DROP TABLE teams; --`;
+const DOM_XSS = `</script><script>alert("XSS")</script>`;
+
+const TEAM_ALREADY_EXIST_ERROR = "a team with the same name already exists";
+
+const USER = {
+  name: "John Doe",
+  email: "john.doe@email.com",
+  address: {
+    street: "123 Main Street",
+    city: "New York",
+    zipCode: "12345",
+  },
+  hiringDate: "2021/01/01",
+  jobTitle: "Software Engineer",
+};
+
+test("should not allow to create a new team with an existing team name", async ({
+  page,
+}: {
+  page: Page;
+}) => {
+  // Create a new team
+  await createNewTeam(page, NEW_TEAM_NAME);
+  // Try to create a new team with the same name
+  await createNewTeam(page, EXISTING_TEAM_NAME);
+
+  // Assert that the error message is visible
+  const locator = page.getByText(TEAM_ALREADY_EXIST_ERROR);
+  await expect(locator).toBeVisible();
+});
+
+test("should not allow SQL injection", async ({ page }: { page: Page }) => {
+  // Create a new team
+  await createNewTeam(page, SQL_INJECTION);
+
+  // Try to create a new team with the same name
+  await createNewTeam(page, SQL_INJECTION);
+
+  // Assert that the error message is visible
+  const locator = page.getByText(TEAM_ALREADY_EXIST_ERROR);
+  await expect(locator).toBeVisible();
+});
+
+test("should not allow DOM XSS", async ({ page }: { page: Page }) => {
+  // Create a new team
+  await createNewTeam(page, DOM_XSS);
+
+  // Try to create a new team with the same name
+  await createNewTeam(page, DOM_XSS);
+
+  // Assert that the error message is visible
+  const locator = page.getByText(TEAM_ALREADY_EXIST_ERROR);
+  await expect(locator).toBeVisible();
+});
+
+test("should not allow to delete a team containing players", async ({
+  page,
+}: {
+  page: Page;
+}) => {
+  // Create a new team
+  await createNewTeam(page, NEW_TEAM_NAME);
+  // Create a new user
+  await createNewUser(page, USER);
+
+  // Add the user to the team
+  // Try to delete the team
+
+  // Assert that the error message is visible
+});
---
+++ utils/index.ts
@@ -0,0 +1,58 @@
+import { type Page } from "@playwright/test";
+
+type UserInformation = {
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+    zipCode: string;
+  };
+  hiringDate: string;
+  jobTitle: string;
+};
+
+/**
+ *
+ * Create a new team
+ * @param page
+ * @param teamName
+ */
+export async function createNewTeam(
+  page: Page,
+  teamName: string
+): Promise<void> {
+  await page.goto("https://c.hr.dmerej.info/add_team");
+  await page.getByPlaceholder("Name").click();
+  await page.getByPlaceholder("Name").fill(teamName);
+  await page.getByRole("button", { name: "Add" }).click();
+}
+
+/**
+ * Create a new user
+ * @param page
+ * @param userInformation
+ */
+export async function createNewUser(
+  page: Page,
+  userInformation: UserInformation
+): Promise<void> {
+  await page.goto("https://c.hr.dmerej.info/add_employee");
+  await page.getByPlaceholder("Name").click();
+  await page.getByPlaceholder("Name").fill(userInformation.name);
+  await page.getByPlaceholder("Name").press("Tab");
+  await page.getByPlaceholder("Email").fill(userInformation.email);
+  await page.getByPlaceholder("Email").press("Tab");
+  await page.locator("#id_address_line1").fill(userInformation.address.street);
+  await page.locator("#id_address_line1").press("Tab");
+  await page.locator("#id_address_line2").press("Tab");
+  await page.getByPlaceholder("City").fill(userInformation.address.city);
+  await page.getByPlaceholder("City").press("Tab");
+  await page.getByPlaceholder("Zip code").fill(userInformation.address.zipCode);
+  await page.getByPlaceholder("Zip code").press("Tab");
+  await page.getByPlaceholder("Hiring date").fill(userInformation.hiringDate);
+  await page.getByPlaceholder("Hiring date").press("Tab");
+  await page.getByPlaceholder("Job title").fill(userInformation.jobTitle);
+  await page.getByPlaceholder("Job title").press("Tab");
+  await page.getByRole("button", { name: "Add" }).press("Enter");
+}
