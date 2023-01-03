eb87ad4babf5e0d1da57d3fea0a1d7f06556def6
NicolasCrssx <nicolas.cresseaux@gmail.com>
2023-01-03 11:53:59 +0100

    Test : Create an employee and updating his infos

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -32,3 +32,33 @@ test("Should create a group", async ({ page }) => {
   await page.getByRole("button", { name: "Add" }).click();
   await expect(page.locator("table > tbody > tr:nth-child(1) > td")).toContainText(["Dev"]);
 });
+
+
+test('Create an employee and updating his infos', async ({ page }) => {
+
+  await page.goto('https://c.hr.dmerej.info/add_employee');
+
+  await page.goto('https://c.hr.dmerej.info/');
+  await page.getByRole('link', { name: 'Add new employee' }).click();
+  await page.getByPlaceholder('Name').fill('Jean Dupont');
+  await page.getByPlaceholder('Email').fill('jean.dupont@gmail.com');
+  await page.locator('#id_address_line1').fill('6 rue de la pompe');
+  await page.getByPlaceholder('City').fill('Paris');
+  await page.getByPlaceholder('Zip code').fill('75012');
+  await page.getByPlaceholder('Hiring date').fill('2023-01-01');
+  await page.getByPlaceholder('Job title').fill('Boss');
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByRole('row', { name: 'Jean Dupont jean.dupont@gmail.com no Edit Delete' }).getByRole('link', { name: 'Edit' }).click();
+  await page.getByRole('link', { name: 'Update basic info' }).click();
+  await page.getByPlaceholder('Name').fill('Jean Dupond');
+  await page.getByPlaceholder('Email').fill('jean.dupond@gmail.com');
+  await page.getByRole('button', { name: 'Update' }).click();
+
+  await page.goto('https://c.hr.dmerej.info/employees');
+
+  await expect(page.locator("table > tbody > tr:nth-child(1) > td")).toContainText([
+    "Jean Dupond",
+    "jean.dupond@gmail.com",
+  ]);
+
+});
